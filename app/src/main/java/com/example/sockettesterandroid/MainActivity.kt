e4962fe748709551d25dce2d06c734fa32db8da3
jmguillen1996 <josefmguillen@gmail.com>
2024-11-26 11:31:44 +0800

    Setup android entry point

--- app/src/main/java/com/example/sockettesterandroid/MainActivity.kt
+++ app/src/main/java/com/example/sockettesterandroid/MainActivity.kt
@@ -6,24 +6,30 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import com.example.sockettesterandroid.ui.theme.SocketTesterAndroidTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         enableEdgeToEdge()
         setContent {
             SocketTesterAndroidTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    color = MaterialTheme.colorScheme.background,
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    AppEntryPoint()
                 }
             }
         }
@@ -31,17 +37,6 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun AppEntryPoint() {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SocketTesterAndroidTheme {
-        Greeting("Android")
-    }
 }
