9158c71b65b52788d9b7db37bfc126bf21b71fb5
jmguillen1996 <josefmguillen@gmail.com>
2024-11-26 14:22:56 +0800

    Created splash basic UI

--- app/src/main/java/com/example/sockettesterandroid/features/presentation/screen/splash/SplashScreen.kt
+++ app/src/main/java/com/example/sockettesterandroid/features/presentation/screen/splash/SplashScreen.kt
@@ -1,15 +1,59 @@
 package com.example.sockettesterandroid.features.presentation.screen.splash
 
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
+import com.example.sockettesterandroid.core.navigation.Routes
 import com.example.sockettesterandroid.features.presentation.screen.splash.common.SplashViewModel
+import kotlinx.coroutines.flow.collectLatest
 
 @Composable
 fun SplashScreen(
     navController: NavController,
     viewModel: SplashViewModel = hiltViewModel(),
 ) {
+    viewModel.initialize()
 
+    LaunchedEffect(key1 = true) {
+        viewModel.uiEvents.collectLatest { events ->
+            when (events) {
+                is SplashViewModel.UIEvents.GoToMainScreen -> {
+                    navController.popBackStack()
+                    navController.navigate(Routes.MAIN)
+                }
+            }
+        }
+    }
+
+    Scaffold(
+        modifier = Modifier.imePadding(),
+        content = { innerPadding ->
+            Surface(
+                modifier = Modifier.padding(innerPadding)
+            ) {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator()
+                }
+            }
+        }
+    )
 }
--- app/src/main/java/com/example/sockettesterandroid/features/presentation/screen/splash/common/SplashViewModel.kt
+++ app/src/main/java/com/example/sockettesterandroid/features/presentation/screen/splash/common/SplashViewModel.kt
@@ -1,10 +1,28 @@
 package com.example.sockettesterandroid.features.presentation.screen.splash.common
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import javax.inject.Inject
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @HiltViewModel
 class SplashViewModel @Inject constructor() : ViewModel() {
 
+    sealed class UIEvents {
+        data object GoToMainScreen : UIEvents()
+    }
+
+    private val _uiEvents = MutableSharedFlow<UIEvents>()
+    val uiEvents = _uiEvents.asSharedFlow()
+
+    fun initialize() {
+        viewModelScope.launch {
+            delay(2000) // Wait for 2 seconds
+            _uiEvents.emit(UIEvents.GoToMainScreen)
+        }
+    }
 }
